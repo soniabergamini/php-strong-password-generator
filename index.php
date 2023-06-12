e81ef9dfec377e205987d9ee7ae4b919724dccc2
soniabergamini <sounia96@outlook.it>
2023-06-12 19:11:10 +0200

    feat(main): add "GetPassword" function

--- index.php
+++ index.php
@@ -1,7 +1,48 @@
 <!-- DATA -->
 <?php
+$alphabetUpper = getChar('A', 'Z');
+$alphabetLower = getChar('a', 'z');
+$numbers = getChar('0', '9');
+$simbols = ["|", "!", "£", "$", "%", "&", "/", "(", ")", "=", "?", "^", "'", "+", "*", "]", "[", "@", "#", "<", ">"];
 $pswLength = $_GET["pswLen"] ?? "";
-var_dump($pswLength)
+var_dump($pswLength);
+$finalPass = getPassword($pswLength, $alphabetUpper, $alphabetLower, $numbers, $simbols);
+var_dump($finalPass);
+?>
+
+<!-- LOGIC -->
+<?php
+
+// Return Aplhabet & Numbers Arrays
+function getChar($min, $max) {
+    foreach (range($min, $max) as $elements) {
+        $array[] = $elements;
+    };
+    return $array;
+}
+
+// Return New Password
+function getPassword($pswLength, $alphabetUpper, $alphabetLower, $numbers, $simbols) {
+    $pwdGen = "";
+
+    // Create array with allowed characters
+    $allChar = [];
+    $allChar = array_merge($allChar, $alphabetUpper);
+    $allChar = array_merge($allChar, $alphabetLower);
+    $allChar = array_merge($allChar, $numbers);
+    $allChar = array_merge($allChar, $simbols);
+    $allCharLen = (count($allChar))-1; // 82
+
+    // Return a string with casual characters (numbers, letters, symbols)
+    while(strlen($pwdGen) < $pswLength) {
+        $casualChar = rand(0, $allCharLen);
+        $pwdGen .= $allChar[$casualChar];
+    }
+
+    // Return Password
+    return $pwdGen;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -39,12 +80,27 @@ var_dump($pswLength)
         <p>Generate your secure password by setting the parameters below:</p>
     </header>
 
-    <!-- Password Generator Form -->
-    <main class="flex justify-center border mx-auto max-w-3xl p-10">
+    <!-- Password Generator Form & Result-->
+    <main class="flex flex-col justify-center border mx-auto max-w-3xl p-10">
+
+        <!-- Password Generator Form -->
         <form action="index.php" method="get" class="w-full">
-            <label for="pswLen">Password length (between 5 - 30):</label>
+            <label for="pswLen" class="font-bold">Password Length (between 5 - 30):</label>
             <input type="number" name="pswLen" id="pswLen" min="5" max="30" placeholder="10" class="w-[50%] ml-5 text-colBlue px-3 font-bold">
+            <div class="mt-6">
+                <button type="submit" class="font-bold bg-colBlue px-4 py-1 hover:bg-colGreen">GENERATE</button>
+                <button type="reset" class="font-bold bg-gray-400 px-4 py-1 hover:bg-colRed ml-2">CLEAR</button>
+            </div>
         </form>
+
+        <!-- Generated Password -->
+        <div class="bg-colPink py-3 px-3 text-black mt-7">
+            <p class="text-md">
+                <span>Your Password:</span>
+                <strong class="text-colGreen text-lg"><?php echo $finalPass ?></strong>
+            </p>
+        </div>
+
     </main>
 
 </body>
